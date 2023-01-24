f52d23d1862e3fb2ad0dbd71707b9dd6fb6b8034
dafifah <123436668+dafifah@users.noreply.github.com>
2023-01-24 11:27:10 +0700

    Create 300x250.js

---
+++ 300x250.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'ef8b6522354acb78270ffe865a7d9d09',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/ef8b6522354acb78270ffe865a7d9d09/invoke.js"></scr' + 'ipt>');
+</script>
