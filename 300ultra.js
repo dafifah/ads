641e872b15792661d7952113036124afe26c876d
dafifah <123436668+dafifah@users.noreply.github.com>
2023-01-31 20:48:52 +0700

    Create 300ultra.js

---
+++ 300ultra.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '671a3e5eb059e41e289ed998a436c861',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/671a3e5eb059e41e289ed998a436c861/invoke.js"></scr' + 'ipt>');
