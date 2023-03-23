50e86025faf79f2532c1bb37fbde89e32457ab8e
dafifah <123436668+dafifah@users.noreply.github.com>
2023-03-23 15:03:23 +0700

    Update 300x250.js

--- 300x250.js
+++ 300x250.js
@@ -1,8 +1,8 @@
 	atOptions = {
-		'key' : 'ef8b6522354acb78270ffe865a7d9d09',
+		'key' : 'f49503a34d574b338e90252d83080e33',
 		'format' : 'iframe',
 		'height' : 250,
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/ef8b6522354acb78270ffe865a7d9d09/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/f49503a34d574b338e90252d83080e33/invoke.js"></scr' + 'ipt>');
