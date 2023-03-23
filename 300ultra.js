a45cd14b863d62ae0f01825ada7100810f3e66b1
dafifah <123436668+dafifah@users.noreply.github.com>
2023-03-23 15:08:38 +0700

    Update 300ultra.js

--- 300ultra.js
+++ 300ultra.js
@@ -1,8 +1,8 @@
 	atOptions = {
-		'key' : '671a3e5eb059e41e289ed998a436c861',
+		'key' : 'f49503a34d574b338e90252d83080e33',
 		'format' : 'iframe',
 		'height' : 250,
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/671a3e5eb059e41e289ed998a436c861/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/f49503a34d574b338e90252d83080e33/invoke.js"></scr' + 'ipt>');
