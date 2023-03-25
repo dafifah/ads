d3f9fe37a5399759ed068c0d9b7d2c1150b8046b
dafifah <123436668+dafifah@users.noreply.github.com>
2023-03-25 08:23:24 +0700

    Create 728color.js

---
+++ 728color.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '6d0938986a3393221729430fae6e21c9',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/6d0938986a3393221729430fae6e21c9/invoke.js"></scr' + 'ipt>');
