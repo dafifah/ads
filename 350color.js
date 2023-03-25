99a40b83e0f86794cd2f3be70016c7515b1ded7b
dafifah <123436668+dafifah@users.noreply.github.com>
2023-03-25 08:22:54 +0700

    Create 350color.js

---
+++ 350color.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '83e4e82b41aaf063bfacc84960b64071',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/83e4e82b41aaf063bfacc84960b64071/invoke.js"></scr' + 'ipt>');
