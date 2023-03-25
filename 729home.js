e774ba7e8f9136732011c3a3f7529672b9079993
dafifah <123436668+dafifah@users.noreply.github.com>
2023-03-25 08:26:18 +0700

    Create 729home.js

---
+++ 729home.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'b5daafd76bc4767b5cc4e17d384e6534',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/b5daafd76bc4767b5cc4e17d384e6534/invoke.js"></scr' + 'ipt>');
