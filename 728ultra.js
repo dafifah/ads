c573992d02ca699f1cc0d91d7872af2f50acc279
dafifah <123436668+dafifah@users.noreply.github.com>
2023-03-25 08:31:40 +0700

    Create 728ultra.js

---
+++ 728ultra.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'c68b16a617eab49c7085e3776944d4aa',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/c68b16a617eab49c7085e3776944d4aa/invoke.js"></scr' + 'ipt>');
