7e927720208b7ff995c3c51be9ca68bc46c7f270
dafifah <123436668+dafifah@users.noreply.github.com>
2023-03-25 08:34:19 +0700

    Create 728car.js

---
+++ 728car.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '711881683313312630000a8d8c4b7362',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/711881683313312630000a8d8c4b7362/invoke.js"></scr' + 'ipt>');
