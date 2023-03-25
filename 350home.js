8a1715f29e0f7b7b527d6b1f4ce7ac51e22bf31f
dafifah <123436668+dafifah@users.noreply.github.com>
2023-03-25 08:24:49 +0700

    Create 350home.js

---
+++ 350home.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'f76f3fc2f167a2a0052fcd127d737c33',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/f76f3fc2f167a2a0052fcd127d737c33/invoke.js"></scr' + 'ipt>');
