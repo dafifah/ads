e3a7f655395de32a056b89b934147f3eeeff03fc
dafifah <123436668+dafifah@users.noreply.github.com>
2023-03-25 08:33:30 +0700

    Create 300car.js

---
+++ 300car.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'aaca6621679abebd730f08fcbe796a9d',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/aaca6621679abebd730f08fcbe796a9d/invoke.js"></scr' + 'ipt>');
