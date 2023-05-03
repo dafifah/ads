e0439975f75982a25ecf15baba4affa4146935db
dafifah <123436668+dafifah@users.noreply.github.com>
2023-05-03 11:50:05 +0700

    Create 728sinar.js

---
+++ 728sinar.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'b904ad74094e28e1dec16b50b2382c7e',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/b904ad74094e28e1dec16b50b2382c7e/invoke.js"></scr' + 'ipt>');
