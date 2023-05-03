06aaa4f45aa659b3a063f7d855eb3d78e5c3a945
dafifah <123436668+dafifah@users.noreply.github.com>
2023-05-03 11:48:03 +0700

    Create 300sinar.js

---
+++ 300sinar.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'f49503a34d574b338e90252d83080e33',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/f49503a34d574b338e90252d83080e33/invoke.js"></scr' + 'ipt>');
