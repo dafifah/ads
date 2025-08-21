bf7068aee211fdf197ab69c6c3f7a15c9158b57f
dafifah <123436668+dafifah@users.noreply.github.com>
2025-08-21 13:21:50 +0700

    Update 350color.js

--- 350color.js
+++ 350color.js
@@ -5,4 +5,4 @@
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/83e4e82b41aaf063bfacc84960b64071/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformanceformat.com/83e4e82b41aaf063bfacc84960b64071/invoke.js"></scr' + 'ipt>');
