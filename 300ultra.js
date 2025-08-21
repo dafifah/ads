e4c91cdb41cfe8358cceefad72d07cfbfda13e02
dafifah <123436668+dafifah@users.noreply.github.com>
2025-08-21 13:15:36 +0700

    Update 300ultra.js

--- 300ultra.js
+++ 300ultra.js
@@ -5,4 +5,4 @@
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/671a3e5eb059e41e289ed998a436c861/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformanceformat.com/671a3e5eb059e41e289ed998a436c861/invoke.js"></scr' + 'ipt>');
