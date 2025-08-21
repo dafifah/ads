08bb7bff8eec2e9051a14e7711d33bea0a95f7e4
dafifah <123436668+dafifah@users.noreply.github.com>
2025-08-21 13:19:21 +0700

    Update 728x90.js

--- 728x90.js
+++ 728x90.js
@@ -5,4 +5,4 @@
 		'width' : 728,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/6d13540907c7d9a1ba8cb1427c1ff4da/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformanceformat.com/6d13540907c7d9a1ba8cb1427c1ff4da/invoke.js"></scr' + 'ipt>');
