f4281922ce229340c27a0223c4939b3d6c4602a9
dafifah <123436668+dafifah@users.noreply.github.com>
2025-08-21 13:18:35 +0700

    Update 300x250.js

--- 300x250.js
+++ 300x250.js
@@ -5,4 +5,4 @@
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/45b857d4c9c51b762db438e4d99e4ee0/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformanceformat.com/45b857d4c9c51b762db438e4d99e4ee0/invoke.js"></scr' + 'ipt>');
