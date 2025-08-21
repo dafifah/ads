d54f7368a1630bb6548982f1c0220126e9112762
dafifah <123436668+dafifah@users.noreply.github.com>
2025-08-21 13:21:08 +0700

    Update 728car.js

--- 728car.js
+++ 728car.js
@@ -5,4 +5,4 @@
 		'width' : 728,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/711881683313312630000a8d8c4b7362/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformanceformat.com/711881683313312630000a8d8c4b7362/invoke.js"></scr' + 'ipt>');
