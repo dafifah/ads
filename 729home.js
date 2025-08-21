06f07f2b0681d0abbb8f3f43ab3e8ef0c961d9a4
dafifah <123436668+dafifah@users.noreply.github.com>
2025-08-21 13:23:26 +0700

    Update 729home.js

--- 729home.js
+++ 729home.js
@@ -5,4 +5,4 @@
 		'width' : 728,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/b5daafd76bc4767b5cc4e17d384e6534/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformanceformat.com/b5daafd76bc4767b5cc4e17d384e6534/invoke.js"></scr' + 'ipt>');
