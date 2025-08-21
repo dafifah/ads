ff223b22ffb27cf3189525a3fab3de213a77a82c
dafifah <123436668+dafifah@users.noreply.github.com>
2025-08-21 13:22:13 +0700

    Update 728color.js

--- 728color.js
+++ 728color.js
@@ -5,4 +5,4 @@
 		'width' : 728,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/6d0938986a3393221729430fae6e21c9/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformanceformat.com/6d0938986a3393221729430fae6e21c9/invoke.js"></scr' + 'ipt>');
