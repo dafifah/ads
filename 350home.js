cd7cafb251c72ed90c9cf17ea80abb0313b46694
dafifah <123436668+dafifah@users.noreply.github.com>
2025-08-21 13:22:56 +0700

    Update 350home.js

--- 350home.js
+++ 350home.js
@@ -5,4 +5,4 @@
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/f76f3fc2f167a2a0052fcd127d737c33/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformanceformat.com/f76f3fc2f167a2a0052fcd127d737c33/invoke.js"></scr' + 'ipt>');
