a2301f214284404b3a884556eb6ea0b92b49423a
dafifah <123436668+dafifah@users.noreply.github.com>
2025-08-21 13:16:21 +0700

    Update 728ultra.js

--- 728ultra.js
+++ 728ultra.js
@@ -5,4 +5,4 @@
 		'width' : 728,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/c68b16a617eab49c7085e3776944d4aa/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformanceformat.com/c68b16a617eab49c7085e3776944d4aa/invoke.js"></scr' + 'ipt>');
